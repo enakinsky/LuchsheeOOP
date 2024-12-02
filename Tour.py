e615c16acad9dbc5c3d95c6b9fb1805c356f6037
Erik <113479062+enakinsky@users.noreply.github.com>
2024-12-02 17:28:11 +0300

    Update Tour.py

--- Tour.py
+++ Tour.py
@@ -112,24 +112,9 @@ if __name__ == "__main__":
             duration=5,
             climat="Субтропики"
         )
-        client = Client(
-            first_name="Иван",
-            last_name="Иванов",
-            middle_name="Иванович",
-            phone_number="79991112233",
-            address="ул. Стасова, д.10"
-        )
+       
 
-        reservation = Reservation(
-            client=client,
-            tour=tour,
-            final_price=Decimal("12000.00"),
-            discount=Decimal("500.00"),
-            date=date(2023, 12, 31)
-        )
-        # print(client)
         print(tour)
-        # print(loan)
 
     except ValueError as e:
         print("Error:", e)
