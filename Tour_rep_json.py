1704daa9ed158be2f0123d2654df187185d258b2
Erik <113479062+enakinsky@users.noreply.github.com>
2024-12-14 10:19:56 +0300

    Lab2.1 реализация класса json

---
+++ Tour_rep_json.py
@@ -0,0 +1,47 @@
+import json
+import os
+from Tour import Tour
+from TourRepository import TourRepository
+from TourRepositoryStrategy import TourRepFileStrategy
+
+class JsonTourRepFileStrategy(TourRepFileStrategy):
+    def __init__(self, file_path: str):
+        self.file_path = file_path
+
+    def read(self):
+        if not os.path.exists(self.file_path):
+            return []
+        with open(self.file_path, 'r', encoding='utf-8') as file:
+            return json.load(file)
+
+    def write(self, data):
+        with open(self.file_path, 'w', encoding='utf-8') as file:
+            json.dump(data, file, ensure_ascii=False, indent=4)
+
+    def display(self):
+        data = self.read()
+        for item in data:
+            print(item)
+
+# Определяем путь к файлу JSON
+strategy = JsonTourRepFileStrategy('tours.json')
+
+
+# Создание репозитория с использованием стратегии JSON
+json_repository = TourRepository(strategy)
+
+new_tour = Tour.create_new_tour(
+            name="Золотое кольцо Абхазии",
+            description="Однодневная экскурсия по всем достопримечательностям",
+            price=2000,
+            duration=5,
+            climat="Субтропики"
+        )
+
+for tour in json_repository.get_k_n_short_list(1,1):
+    print(tour)
+
+
+# Отображение обновленного списка продуктов
+print("\nUpdated tours in JSON file:")
+strategy.display()
