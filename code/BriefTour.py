462ed607ea05fd3b17fe0e681a66356198b6080a
Erik <113479062+enakinsky@users.noreply.github.com>
2024-12-16 11:27:27 +0300

    lab 2.4 добавление tour code

--- code/BriefTour.py
+++ code/BriefTour.py
@@ -1,8 +1,10 @@
 class BriefTour:
-    def __init__(self, tour_id: int = 0, name: str = "", price: int = 10):
+    def __init__(self, tour_id: int = 0, name: str = "", price: int = 1, tour_code: str = ""):
         self.tour_id = tour_id
         self.name = name
         self.price = price
+        self.tour_code = tour_code
+
 
     @property
     def tour_id(self):
@@ -34,8 +36,20 @@ class BriefTour:
             raise ValueError("Price must be a non-negative integer.")
         self._price = value
 
+    @property
+    def tour_code(self):
+        return self.tour_code
+
+    @product_code.setter
+    def tour_code(self, value: str):
+        if not (isinstance(value, str) and value.isdigit() and len(value) == 6):
+            raise ValueError("Tour code must be a string of 6 digits.")
+        self.tour_code = value
+        
     def __eq__(self, other):
         if not isinstance(other, BriefTour):
             return False
         return (self.name == other.name)
-
+        
+    def brief(self):
+        return f"BriefTour(tour_id={self.tour_id}, name={self.name}, price={self.price})"
