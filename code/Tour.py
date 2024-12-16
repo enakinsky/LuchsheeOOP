ff6f125b6837da9d74943468953095497ef144ac
Erik <113479062+enakinsky@users.noreply.github.com>
2024-12-16 11:19:44 +0300

    lab2.4 добавление создания из yaml и dict

--- code/Tour.py
+++ code/Tour.py
@@ -5,7 +5,7 @@ from BriefTour import BriefTour
 
 class Tour(BriefTour):
     def __init__(self, tour_id: int = 0, name: str = "", description: str = "", price: int = 0,
-                 duration: int = 0, climat: str = ""):
+                 duration: int = 0, climat: str = "", tour_code: str = ""):
         super().__init__(tour_id, name, price)
         self.description = description
         self.duration = duration
@@ -42,19 +42,19 @@ class Tour(BriefTour):
         self._climat = value
 
     @classmethod
-    def create_new_tour(cls, name: str, description: str, price: int, duration: int, climat: str):
-        return cls(name=name, description=description, price=price, duration=duration, climat=climat)
+    def create_new_tour(cls, name: str, description: str, price: int, duration: int, climat: str, tour_code: str):
+        return cls(name=name, description=description, price=price, duration=duration, climat=climat, tour_code=tour_code)
 
     @classmethod
     def update_existing_tour(cls, tour_id: int, name: str, description: str, price: int, duration: int,
-                             climat: str):
-        return cls(tour_id=tour_id, name=name, description=description, price=price, duration=duration, climat=climat)
+                             climat: str, tour_code: str):
+        return cls(tour_id=tour_id, name=name, description=description, price=price, duration=duration, climat=climat, tour_code=tour_code)
 
     @classmethod
     def create_from_string(cls, tour_string: str):
         parts = tour_string.split(",")
-        if len(parts) != 6:
-            raise ValueError("Invalid tour string format. Expected 6 comma-separated values.")
+        if len(parts) != 7:
+            raise ValueError("Invalid tour string format. Expected 7 comma-separated values.")
         try:
             return cls(
                 tour_id=int(parts[0].strip()),
@@ -62,7 +62,8 @@ class Tour(BriefTour):
                 description=parts[2].strip(),
                 price=int(parts[3].strip()),
                 duration=int(parts[4].strip()),
-                climat=parts[5].strip()
+                climat=parts[5].strip(),
+                tour_code=parts[5].strip()
             )
         except ValueError as e:
             raise ValueError("Invalid number format in tour string.") from e
@@ -74,9 +75,22 @@ class Tour(BriefTour):
             tour_id=data['tour_id'],
             name=data['name'],
             description=data['description'],
-            price=int(data['price']),
+            price=data['price'],
             duration=data['duration'],
-            climat=data['climat']
+            climat=data['climat'],
+            tour_code=data['tour_code']
+        )
+
+    @classmethod
+    def create_from_dict(cls, data: dict):
+        return cls(
+            tour_id=data.get('tour_id'),
+            name=data['name'],
+            description=data['description'],
+            price=data['price'],
+            duration=data['stock_quantity'],
+            climat=data['material'],
+            tour_code=data['tour_code']
         )
 
     def to_json(self) -> str:
@@ -86,24 +100,44 @@ class Tour(BriefTour):
             'description': self.description,
             'price': str(self.price),
             'duration': self.duration,
-            'climat': self.climat
+            'climat': self.climat,
+            'tour_code': self.tour_code
         }, ensure_ascii=False)
 
-    def __str__(self):
-        return f"tour(tourId={self.tour_id}, name='{self.name}', description='{self.description}', price={self.price}, duration={self.duration}, climat='{self.climat}')"
-
-
-if __name__ == "__main__":
-    try:
-        tour1 = Tour.create_new_tour(
-            name="Золотое кольцо Абхазии",
-            description="Однодневная экскурсия по всем достопримечательностям",
-            price=2000,
-            duration=5,
-            climat="Субтропики"
+     @classmethod
+    def create_from_yaml(cls, yaml_string: str):
+        data = yaml.safe_load(yaml_string)
+        return cls(
+            tour_id=data.get('tour_id'),
+            name=data['name'],
+            description=data['description'],
+            price=data['price'],
+            duration=data['duration'],
+            climat=data['climat'],
+            tour_code=data['tour_code']
         )
-        tour1.name = "qwe1"
-        print(tour1)
 
-    except ValueError as e:
-        print("Error:", e)
+    def to_yaml(self) -> str:
+        return yaml.dump({
+            'tour_id': self.tour_id,
+            'name': self.name,
+            'description': self.description,
+            'price': self.price,
+            'duration': self.duration,
+            'climat': self.climat,
+            'tour_code': self.tour_code
+        }, allow_unicode=True)
+
+    def to_dict(self) -> dict:
+        return {
+            "tour_id": self.tour_id,
+            "name": self.name,
+            "description": self.description,
+            "price": self.price,  
+            "duration": self.duration,
+            "climat": self.climat,
+            "tour_code": self.tour_code
+        }
+
+    def __str__(self):
+        return f"tour(tourId={self.tour_id}, name='{self.name}', description='{self.description}', price={self.price}, duration={self.duration}, climat='{self.climat}', tourCode='{self.tour_code}')"
