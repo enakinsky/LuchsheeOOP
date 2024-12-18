1976da169d799178b2d930907edf2f20ee5fc13d
Erik <113479062+enakinsky@users.noreply.github.com>
2024-12-18 15:52:52 +0300

    Lab 2.3 добавление отображение

--- code/Tour_rep_json.py
+++ code/Tour_rep_json.py
@@ -22,12 +22,9 @@ class JsonTourRepFileStrategy(TourRepFileStrategy):
         data = self.read()
         for item in data:
             print(item)
-
-# Определяем путь к файлу JSON
+            
 strategy = JsonTourRepFileStrategy('tours.json')
 
-
-# Создание репозитория с использованием стратегии JSON
 json_repository = TourRepository(strategy)
 
 new_tour = Tour.create_new_tour(
@@ -43,6 +40,5 @@ for tour in json_repository.get_k_n_short_list(1,1):
     print(tour)
 
 
-# Отображение обновленного списка продуктов
 print("\nUpdated tours in JSON file:")
 strategy.display()
